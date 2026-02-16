31d970cfbbfadf68840f0ca628a6ed8bdf3636aa
Luca Baldini <luca.baldini@pi.infn.it>
2026-02-16 14:51:12 +0100

    Initial import.

---
+++ macro/middle_square.py
@@ -0,0 +1,255 @@
+
+
+"""
+Endpoint: 0
+42 -> [76, 77, 92, 46, 11, 12, 14, 19, 36, 29, 84, 5, 2, 0] 14
+69 -> [76, 77, 92, 46, 11, 12, 14, 19, 36, 29, 84, 5, 2, 0] 14
+76 -> [77, 92, 46, 11, 12, 14, 19, 36, 29, 84, 5, 2, 0] 13
+77 -> [92, 46, 11, 12, 14, 19, 36, 29, 84, 5, 2, 0] 12
+89 -> [92, 46, 11, 12, 14, 19, 36, 29, 84, 5, 2, 0] 12
+92 -> [46, 11, 12, 14, 19, 36, 29, 84, 5, 2, 0] 11
+31 -> [96, 21, 44, 93, 64, 9, 8, 6, 3, 0] 10
+46 -> [11, 12, 14, 19, 36, 29, 84, 5, 2, 0] 10
+63 -> [96, 21, 44, 93, 64, 9, 8, 6, 3, 0] 10
+11 -> [12, 14, 19, 36, 29, 84, 5, 2, 0] 9
+81 -> [56, 13, 16, 25, 62, 84, 5, 2, 0] 9
+87 -> [56, 13, 16, 25, 62, 84, 5, 2, 0] 9
+96 -> [21, 44, 93, 64, 9, 8, 6, 3, 0] 9
+12 -> [14, 19, 36, 29, 84, 5, 2, 0] 8
+21 -> [44, 93, 64, 9, 8, 6, 3, 0] 8
+38 -> [44, 93, 64, 9, 8, 6, 3, 0] 8
+56 -> [13, 16, 25, 62, 84, 5, 2, 0] 8
+13 -> [16, 25, 62, 84, 5, 2, 0] 7
+14 -> [19, 36, 29, 84, 5, 2, 0] 7
+44 -> [93, 64, 9, 8, 6, 3, 0] 7
+54 -> [91, 28, 78, 8, 6, 3, 0] 7
+16 -> [25, 62, 84, 5, 2, 0] 6
+17 -> [28, 78, 8, 6, 3, 0] 6
+19 -> [36, 29, 84, 5, 2, 0] 6
+37 -> [36, 29, 84, 5, 2, 0] 6
+41 -> [68, 62, 84, 5, 2, 0] 6
+58 -> [36, 29, 84, 5, 2, 0] 6
+91 -> [28, 78, 8, 6, 3, 0] 6
+93 -> [64, 9, 8, 6, 3, 0] 6
+25 -> [62, 84, 5, 2, 0] 5
+27 -> [72, 18, 32, 2, 0] 5
+28 -> [78, 8, 6, 3, 0] 5
+36 -> [29, 84, 5, 2, 0] 5
+61 -> [72, 18, 32, 2, 0] 5
+64 -> [9, 8, 6, 3, 0] 5
+68 -> [62, 84, 5, 2, 0] 5
+75 -> [62, 84, 5, 2, 0] 5
+82 -> [72, 18, 32, 2, 0] 5
+9 -> [8, 6, 3, 0] 4
+29 -> [84, 5, 2, 0] 4
+33 -> [8, 6, 3, 0] 4
+43 -> [84, 5, 2, 0] 4
+62 -> [84, 5, 2, 0] 4
+72 -> [18, 32, 2, 0] 4
+78 -> [8, 6, 3, 0] 4
+7 -> [4, 1, 0] 3
+8 -> [6, 3, 0] 3
+18 -> [32, 2, 0] 3
+71 -> [4, 1, 0] 3
+73 -> [32, 2, 0] 3
+84 -> [5, 2, 0] 3
+4 -> [1, 0] 2
+5 -> [2, 0] 2
+6 -> [3, 0] 2
+32 -> [2, 0] 2
+45 -> [2, 0] 2
+55 -> [2, 0] 2
+95 -> [2, 0] 2
+0 -> [0] 1
+1 -> [0] 1
+2 -> [0] 1
+3 -> [0] 1
+Number of sequences ending in 0: 62
+Endpoint: None
+24 -> [57, 24] 2
+57 -> [24, 57] 2
+79 -> [24, 57] 2
+Number of sequences ending in None: 3
+Endpoint: 10
+34 -> [15, 22, 48, 30, 90, 10] 6
+66 -> [35, 22, 48, 30, 90, 10] 6
+15 -> [22, 48, 30, 90, 10] 5
+26 -> [67, 48, 30, 90, 10] 5
+35 -> [22, 48, 30, 90, 10] 5
+65 -> [22, 48, 30, 90, 10] 5
+85 -> [22, 48, 30, 90, 10] 5
+86 -> [39, 52, 70, 90, 10] 5
+22 -> [48, 30, 90, 10] 4
+23 -> [52, 70, 90, 10] 4
+39 -> [52, 70, 90, 10] 4
+59 -> [48, 30, 90, 10] 4
+67 -> [48, 30, 90, 10] 4
+48 -> [30, 90, 10] 3
+52 -> [70, 90, 10] 3
+30 -> [90, 10] 2
+70 -> [90, 10] 2
+10 -> [10] 1
+90 -> [10] 1
+Number of sequences ending in 10: 19
+Endpoint: 50
+50 -> [50] 1
+Number of sequences ending in 50: 1
+Endpoint: 60
+94 -> [83, 88, 74, 47, 20, 40, 60] 7
+83 -> [88, 74, 47, 20, 40, 60] 6
+88 -> [74, 47, 20, 40, 60] 5
+74 -> [47, 20, 40, 60] 4
+47 -> [20, 40, 60] 3
+53 -> [80, 40, 60] 3
+99 -> [80, 40, 60] 3
+20 -> [40, 60] 2
+49 -> [40, 60] 2
+80 -> [40, 60] 2
+97 -> [40, 60] 2
+40 -> [60] 1
+51 -> [60] 1
+60 -> [60] 1
+98 -> [60] 1
+Number of sequences ending in 60: 15
+"""
+
+from matplotlib.patches import Circle
+
+from aptapy.plotting import plt, apply_stylesheet, setup_gca
+
+apply_stylesheet("aptapy-xkcd")
+
+
+_POS_DICT = {}
+_HOR_SPACING = 1.5
+_VERT_SPACING = 1.25
+
+def put(value: int, pos: tuple[float, float], seed: bool = False, label: str = None) -> None:
+    """Put a value at a position on the plot and store the position in a
+    dictionary for later use.
+    """
+    if label is None:
+        label = f"{value}"
+    plt.text(*pos, label, ha='center', va='center', fontsize="small")
+    _POS_DICT[value] = pos
+    if seed:
+        circle = Circle(pos, radius=0.35, edgecolor='black', facecolor='none')
+        plt.gca().add_patch(circle)
+
+def arrow(src: int, dest: int, padding: float = 0.25) -> None:
+    """Connect two values with an arrow.
+    """
+    x1, y1 = _POS_DICT[src]
+    x2, y2 = _POS_DICT[dest]
+    if x1 == x2:
+        padding *= _VERT_SPACING
+        if y1 > y2:
+            padding = -padding
+        y1 += padding
+        y2 -= padding
+    elif y1 == y2:
+        padding *= _HOR_SPACING
+        if x1 > x2:
+            padding = -padding
+        x1 += padding
+        x2 -= padding
+    plt.gca().annotate("", xytext=(x1, y1), xy=(x2, y2), arrowprops=dict(arrowstyle="->"))
+
+def put_right(value: int, reference: int, connect: bool = True):
+    """
+    """
+    x, y = _POS_DICT[reference]
+    put(value, (x + _HOR_SPACING, y))
+    if connect: arrow(reference, value)
+
+def put_left(value: int, reference: int, connect: bool = True):
+    """
+    """
+    x, y = _POS_DICT[reference]
+    put(value, (x - _HOR_SPACING, y))
+    if connect: arrow(value, reference)
+
+def put_above(value: int, reference: int, vspace: float = 1.0, connect: bool = True):
+    """
+    """
+    x, y = _POS_DICT[reference]
+    put(value, (x, y + vspace * _VERT_SPACING))
+    if connect: arrow(value, reference)
+
+def put_below(value: int, reference: int, vspace: float = 1.0, connect: bool = True, label: str = None):
+    """
+    """
+    x, y = _POS_DICT[reference]
+    put(value, (x, y - vspace * _VERT_SPACING), label=label)
+    if connect: arrow(value, reference)
+
+
+plt.figure("middle_square", figsize=(10, 8))
+
+
+ref = 42
+put(ref, (0, 0))
+for val in [76, 77, 92, 46, 11, 12, 14, 19, 36, 29, 84, 5, 2, 0]:
+    put_right(val, ref)
+    ref = val
+put_above(69, 76)
+put_above(89, 92)
+ref = 31
+put_below(ref, 11, vspace=3., connect=False)
+for val in [96, 21, 44, 93, 64, 9, 8, 6, 3]:#, 0]:
+    put_right(val, ref)
+    ref = val
+arrow(3, 0)
+put_below(63, 96)
+ref = 81
+put_above(ref, 12, vspace=2., connect=False)
+for val in [56, 13, 16, 25, 62]:#, 84, 5, 2, 0]
+    put_right(val, ref)
+    ref = val
+arrow(62, 84)
+put_above(87, 56)
+put_below(38, 44)
+ref = 54
+put_below(ref, 93, connect=False)
+for val in [91, 28, 78]:
+    put_right(val, ref)
+    ref = val
+arrow(78, 8)
+put_below(17, 28)
+put_below(37, 36)
+put_above(68, 62)
+put_above(41, 68)
+put_above(58, 36)
+put_below(28, 78)
+put_below(91, 28)
+put_above(93, 64)
+put_above(32, 2)
+put_above(18, 32)
+put_above(72, 18)
+put_above(27, 72)
+put_right(28, 78, connect=False)
+arrow(28, 78)
+put_left(61, 72)
+put_right(75, 62, connect=False)
+arrow(75, 62)
+put_right(82, 72, connect=False)
+arrow(82, 72)
+put_above(33, 8)
+put_below(43, 84)
+put_right(73, 32, connect=False)
+arrow(73, 32)
+put_below(45, 2, label="45,55,95")
+put_right(1, 0, connect=False)
+arrow(1, 0)
+put_right(4, 1, connect=False)
+arrow(4, 1)
+put_right(7, 4, connect=False)
+arrow(7, 4)
+put_above(71, 4)
+
+
+plt.axis("off")
+plt.gca().set_aspect("equal")
+setup_gca(xmin=-0.5, xmax=18 * _HOR_SPACING, ymin=-5 * _VERT_SPACING, ymax=5 * _VERT_SPACING)
+
+plt.show()
