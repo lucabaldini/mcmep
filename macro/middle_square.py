9c9aadac5c5ac6f3e8293c6f52c0f79c210ce3ba
Luca Baldini <luca.baldini@pi.infn.it>
2026-02-17 20:51:17 +0100

    Minor.

--- macro/middle_square.py
+++ macro/middle_square.py
@@ -27,6 +27,7 @@ def put(value: int, pos: tuple[float, float], label: str = None,
         circle = Circle(pos, radius=frame_size, **frame_kwargs)
         plt.gca().add_patch(circle)
     if fixed:
+        frame_size *= 1.15
         square = plt.Rectangle((pos[0] - frame_size, pos[1] - frame_size),
                                width=2*frame_size, height=2*frame_size, **frame_kwargs)
         plt.gca().add_patch(square)
@@ -103,45 +104,18 @@ def put_below(value: int, reference: int, vspace: float = 1.0, connect: bool = T
 plt.figure("middle_square", figsize=(10, 8))
 
 # Sequences ending with 0
-## [42, 76, 77, 92, 46, 11, 12, 14, 19, 36, 29, 84, 5, 2, 0] -> []  (len=15)
-## [69] -> [76, 77, 92, 46, 11, 12, 14, 19, 36, 29, 84, 5, 2, 0]  (len=15)
-## [89] -> [92, 46, 11, 12, 14, 19, 36, 29, 84, 5, 2, 0]  (len=13)
-## [31, 96, 21, 44, 93, 64, 9, 8, 6, 3] -> [0]  (len=11)
-## [63] -> [96, 21, 44, 93, 64, 9, 8, 6, 3, 0]  (len=11)
-## [81, 56, 13, 16, 25, 62] -> [84, 5, 2, 0]  (len=10)
-## [87] -> [56, 13, 16, 25, 62, 84, 5, 2, 0]  (len=10)
-## [38] -> [44, 93, 64, 9, 8, 6, 3, 0]  (len=9)
-## [54, 91, 28, 78] -> [8, 6, 3, 0]  (len=8)
-## [17] -> [28, 78, 8, 6, 3, 0]  (len=7)
-## [37] -> [36, 29, 84, 5, 2, 0]  (len=7)
-## [41, 68] -> [62, 84, 5, 2, 0]  (len=7)
-## [58] -> [36, 29, 84, 5, 2, 0]  (len=7)
-## [27, 72, 18, 32] -> [2, 0]  (len=6)
-## [61] -> [72, 18, 32, 2, 0]  (len=6)
-## [75] -> [62, 84, 5, 2, 0]  (len=6)
-## [82] -> [72, 18, 32, 2, 0]  (len=6)
-## [33] -> [8, 6, 3, 0]  (len=5)
-## [43] -> [84, 5, 2, 0]  (len=5)
-## [7, 4, 1] -> [0]  (len=4)
-## [71] -> [4, 1, 0]  (len=4)
-## [73] -> [32, 2, 0]  (len=4)
-# [45] -> [2, 0]  (len=3)
-# [55] -> [2, 0]  (len=3)
-# [95] -> [2, 0]  (len=3)
-
-
-put(0, (0, 0), fixed=True)
+put(0, (_HOR_SPACING, 0), fixed=True)
 seq(0, 2, 5, 84, 29, 36, 19, 14, 12, 11, 46, 92, 77, 76, 42)
 seq(0, 1, 4, 7, direction="above")
 put_above(69, 76)
-put_below(3, 0, vspace=5.)
+put_below(3, 0, vspace=6.)
 seq(3, 6, 8, 9, 64, 93, 44, 21, 96, 31)
 put_above(63, 96)
 put_above(38, 44)
 put_above(62, 84, vspace=2., seed=False)
 seq(62, 25, 16, 13, 56, 81)
 put_above(87, 56)
-put_below(78, 8, vspace=2.)
+put_below(78, 8, vspace=1.)
 seq(78, 28, 91, 54)
 put_below(17, 28)
 put_above(37, 36)
@@ -162,11 +136,42 @@ x, y = _POS_DICT[55]
 put(45, (x - 0.5 * _HOR_SPACING, y), seed=True)
 put(95, (x + 0.5 * _HOR_SPACING, y), seed=True)
 
+# Sequences ending with 10
+put(10, (-8. * _HOR_SPACING, -4. * _VERT_SPACING), fixed=True)
+seq(10, 90, 30, 48, 22, 15, 34)
+put_below(35, 22, seed=False)
+put_below(66, 35)
+put_below(67, 48, seed=False)
+put_below(26, 67)
+put_above(85, 22)
+xy = _POS_DICT[85]
+put(85, (xy[0] - 0.5 * _HOR_SPACING, xy[1]), seed=True)
+put_above(70, 90, vspace=2., seed=False)
+seq(70, 52, 39, 86)
+put_above(23, 52)
+put_above(59, 48)
+
+# Sequences ending with 60
+put(60, (-7. * _HOR_SPACING, 5. * _VERT_SPACING), fixed=True)
+seq(60, 40, 20, 47, 74, 88, 83, 94)
+put_below(80, 40, seed=False)
+put_below(53, 80)
+put_left(99, 80)
+put_above(49, 40)
+xy = _POS_DICT[49]
+put(97, (xy[0] - 0.5 * _HOR_SPACING, xy[1]), seed=True)
+put_above(51, 60)
+xy = _POS_DICT[51]
+put(98, (xy[0] + 0.5 * _HOR_SPACING, xy[1]), seed=True)
+
+# 50 is an isolated point
+put(50, (-10. * _HOR_SPACING, -8. * _VERT_SPACING), fixed=True, seed=True)
+
 
 plt.axis("off")
 plt.subplots_adjust(left=0., right=1., top=1., bottom=0.)
 plt.gca().set_aspect("equal")
-setup_gca(xmin=-15. * _HOR_SPACING, xmax=2. * _HOR_SPACING,
-          ymin=-9 * _VERT_SPACING, ymax=5 * _VERT_SPACING)
+setup_gca(xmin=-15. * _HOR_SPACING, xmax=3. * _HOR_SPACING,
+          ymin=-9 * _VERT_SPACING, ymax=7 * _VERT_SPACING)
 
 plt.show()
